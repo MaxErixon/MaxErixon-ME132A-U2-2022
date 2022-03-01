48c4a0cc8fe0d72f8615bbeaca51ba6e74ab0d00
Max Erixon <maxerixon112@gmail.com>
2022-03-01 16:21:08 +0100

    Created database of stadiums

---
+++ database.js
@@ -0,0 +1,44 @@
+let stadiumDatabase = [
+    {
+        id: 1,
+        name: "Tele2 Arena",
+        team: "Hammarby IF",
+        city: "Stockholm",
+        capacity: 31000
+    },
+    {
+        id: 2,
+        name: "Camp Nou",
+        team: "FC Barcelona",
+        city: "Barcelona",
+        capacity: 98000
+    },
+    {
+        id: 3,
+        name: "Wanda Metropolitano",
+        team: "Atlético Madrid",
+        city: "Madrid",
+        capacity: 72000
+    },
+    {
+        id: 4,
+        name: "La Bombanera",
+        team: "Boca Juniors",
+        city: "Buenos Aires",
+        capacity: 54000
+    },
+    {
+        id: 5,
+        name: "Estadio do Dragao",
+        team: "FC Porto",
+        city: "Porto",
+        capacity: 50000
+    },
+    {
+        id: 6,
+        name: "Stade Vélodrome",
+        team: "Olympiqe de Marseille",
+        city: "Marseille",
+        capacity: 67000
+    }
+];
